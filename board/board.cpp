e7325bbd042e41067f4cf26f462cfedc088dd27a
chad <chad@816618ff-971e-4f3c-9917-b646dc1eeecf>
2015-08-29 09:09:11 +0000

    Netlist extraction and checking.
    git-svn-id: http://www.cdkersey.com/log/2015.08.21/eeprom_prog@1691 816618ff-971e-4f3c-9917-b646dc1eeecf

--- board/board.cpp
+++ board/board.cpp
@@ -30,22 +30,43 @@ enum layer {
   LAYER_PTH, LAYER_NPTH                           // Thru-holes (plated/non)
 };
 
+double quantize(double x, double step) {
+  x = round(x * (1/step))/step;
+  return x;
+}
+
 struct point {
   point(): x(0), y(0) {}
   point(double x, double y): x(x), y(y) {}
   
   double x, y;
 
-  bool operator==(const point &r) const { return r.x == x && r.y == y; }
-  bool operator!=(const point &r) const { return r.x != x || r.y != y; }
+  bool close(const point &r) const { return d2(r) < THRESH; }
+  bool operator==(const point &r) const { return x == r.x && y == r.y; }
+
+  bool operator!=(const point &r) const { return !operator==(r); }
   point operator+(const point &r) const { return point(x + r.x, y + r.y); }
   point operator-(const point &r) const { return point(x - r.x, y - r.y); }
   
   point operator+=(const point &r) { x += r.x; y += r.y; }
   point operator-=(const point &r) { x -= r.x; y -= r.y; }
 
+  bool operator<(const point &r) const {
+    return (x < r.x) || ((x == r.x) && y < r.y);
+  }
+
+  point neighborhood() const { return point(quantize(x, 1e-4), quantize(y, 1e-4)); }
+
   point scale(double r) const { return point(r*x, r*y); }
+
+  double r2() const { return x*x + y*y; }
+  double d2(const point &r) const { return pow(r.x - x, 2) + pow(r.y - y, 2); }
+  constexpr static double THRESH = 1e-8;
 };
+
+ostream &operator<<(ostream &o, const point &p) {
+  o << '(' << p.x << ", " << p.y << ')';
+}
 
 // The gerber file writer; the graphical output API.
 class gerber {
@@ -136,7 +157,53 @@ private:
   point p;
 };
 
-class pin {
+class net;
+
+class wire {
+public:
+  wire(): n(NULL) { wires.insert(this); }
+  virtual ~wire() { wires.erase(this); }
+  virtual void get_points(set<pair<point, int> > &s) = 0;
+
+  void mark();
+  
+  void set_net(net *p) { n = p; }
+  net *get_net() { return n; }
+
+  static void print_wires();
+  static void expand_nets();
+  static void check_nets();
+
+private:
+  net *n;
+  static set<wire*> wires;
+};
+
+set<wire*> wire::wires;
+
+class net {
+public:
+  net(string name): name(name) { nets.insert(this); }
+  virtual ~net() { nets.erase(this); }
+
+  void add_wire(wire &w) { wires.insert(&w); w.set_net(this); }
+
+  string get_name() { return name; }
+  
+  void mark() { for (auto w : wires) w->mark(); }
+  
+  static void print_nets();
+
+private:
+  set<wire*> wires;
+  string name;
+
+  static set<net*> nets;
+};
+
+set<net*> net::nets;
+
+class pin : public wire {
 public:
   pin() {}
   pin(string name, point p, const set<layer> &l): name(name), p(p), l(l) {}
@@ -147,6 +214,8 @@ public:
   point get_loc() const { return p; }
   string get_name() const { return name; }
 
+  void get_points(set<pair<point, int> > &s);
+  
 private:
   set<layer> l;
   point p;
@@ -159,7 +228,7 @@ public:
   virtual ~component() {}
   string get_name() const { return name; }
   virtual string get_type_name() = 0;
-  const pin &get_pin(string name) const { return pins.find(name)->second; }
+  pin &get_pin(string name) { return pins.find(name)->second; }
 
 protected:
   void add_pin(string name, point loc)
@@ -187,7 +256,7 @@ public:
 };
 
 // Linear (as in non-branching, not necessarily straight) circuit trace.
-class track : public drawable {
+class track : public drawable, public wire {
 public:
   track(int layer_idx, double thickness, double clearance):
     layer_idx(layer_idx), thickness(thickness), clearance(clearance)
@@ -201,6 +270,8 @@ public:
   track &add_point(double x, double y) { return add_point(point(x, y)); }
   
   void draw(int pri, layer l, gerber &g);
+
+  void get_points(set<pair<point, int> > &s);
   
 private:
   int layer_idx;
@@ -209,7 +280,7 @@ private:
 };
 
 // Non-buried (all-layers) via.
-class via : public drawable {
+class via : public drawable, public wire {
 public:
   via(point center, double outer, double inner):
     center(center), outer(outer), inner(inner), clearance(outer*1.25)
@@ -220,6 +291,8 @@ public:
   { add_priority(-50); add_priority(0); }
   
   virtual void draw(int pri, layer l, gerber &g);
+
+  void get_points(set<pair<point, int> > &s);
   
 protected:
   double clearance, outer, inner;
@@ -508,6 +581,164 @@ template <unsigned L, unsigned W, bool V>
 	   1/25.0);
 }
 
+void pin::get_points(set<pair<point, int> > &s) {
+  for (auto x : l)
+    s.insert(make_pair(p, (int)x - LAYER_CU0));
+}
+
+void via::get_points(set<pair<point, int> > &s) {
+  for (unsigned l = 0; l < N_CU; ++l)
+    s.insert(make_pair(center, l));
+}
+
+void track::get_points(set<pair<point, int> > &s) {
+  for (auto &p : points)
+    s.insert(make_pair(p, layer_idx));
+}
+
+void wire::print_wires() {
+  for (auto &w : wires) {
+    set<pair<point, int> > s;
+    w->get_points(s);
+
+    cout << "Wire " << &w << ':';
+    for (auto &x : s)
+      cout << ' ' << x.first << ' ' << x.second;
+    cout << endl;
+  }
+}
+
+void wire::mark() {
+  set<pair<point, int> > s;
+  get_points(s);
+  for (auto &p : s)
+    new text(get_default_font(), LAYER_SILKSCREEN, p.first, "X", 1/80.0);
+}
+  
+
+
+void net::print_nets() {
+  for (auto p : nets) {
+    cout << p->name << ":" << endl;
+    for (auto wp : p->wires)
+      cout << "  " << wp << endl;
+  }
+}
+
+void wire::expand_nets() {
+  map<pair<point, int>, set<wire*> > l; // The location->wire map
+  map<pair<point, int>, net*> g; // The location->net map/graph
+  set<wire*> active;
+
+  // Populate g with initial nets.
+  for (auto w : wires) {
+    set<pair<point, int> > s;
+    w->get_points(s);
+    for (auto &p : s)
+      l[make_pair(p.first.neighborhood(), p.second)].insert(w);
+
+    if (w->get_net()) {
+      active.insert(w);
+      for (auto &p : s) {
+	if (g.count(p) && g[p] != w->get_net()) {
+	  cout << "Warning: Net contention! (build)" << endl;
+	}
+        g[make_pair(p.first.neighborhood(), p.second)] = w->get_net();
+      }
+    }
+  }
+
+
+  
+  // Keep propagating nets until there are none left to propagate.
+  bool change;
+  do {
+    change = false;
+    vector<pair<wire*, net*> > set_nets;
+    set<wire*> next_active;
+
+    for (auto wp : active) {
+      set<pair<point, int> > s;
+      wp->get_points(s);
+      for (auto &p : s) {
+	net *n(wp->get_net());
+
+	for (auto w : l[make_pair(p.first.neighborhood(), p.second)]) {
+	  if (w == wp) continue;
+	  if (w->get_net() && w->get_net() != n)
+            cout << "Warning: Net contention! (propagate)" << endl;
+	  if (w->get_net() == NULL) {
+            set_nets.push_back(make_pair(w, n));
+	    next_active.insert(w);			       
+	  }
+	}
+      }
+    }
+
+    if (set_nets.size()) change = true;
+    for (auto &x : set_nets) {
+      x.second->add_wire(*x.first);
+      set<pair<point, int> > s;
+      x.first->get_points(s);
+      for (auto &y : s)
+	g[make_pair(y.first.neighborhood(), y.second)] = x.second;
+    }
+
+    active = next_active;
+  } while (active.size());
+}
+
+// Report if any nets are split into multiple parts or if there are any wires to
+// which nets are not assigned.
+void wire::check_nets() {
+  expand_nets(); // Won't take much time if it's already been done.
+  
+  map<net*, set<wire*> > g;
+  map<wire*, wire*> tag;
+  map<pair<point, int>, set<wire*> > l;
+
+  for (auto w : wires) {
+    g[w->get_net()].insert(w);
+    tag[w] = w;
+
+    set<pair<point, int> > s;
+    w->get_points(s);
+    for (auto &p : s)
+      l[make_pair(p.first.neighborhood(), p.second)].insert(w);
+  }
+
+  int merges;
+  do {
+    merges = 0;
+    // If there's more than one wire tag at any location, merge their tags.
+    for (auto &p : l) {
+      set<wire*> tags;
+      for(auto x : p.second)
+	tags.insert(tag[x]);
+      if (tags.size() > 1) {
+	++merges;
+	wire *new_tag(*tags.begin());
+	for (auto x : p.second)
+	  tag[x] = new_tag;
+      }
+    }
+  } while (merges);
+
+  for (auto &p : g) {
+    set<wire*> tags;
+    for (auto x : p.second)
+      tags.insert(tag[x]);
+
+    if (p.first) {
+      if (tags.size() > 1)
+        cout << "ERROR: Net " << p.first->get_name() << " in " 
+             << tags.size() << " pieces." << endl;
+    } else {
+      cout << "ERROR: " << tags.size() << " orphan nets." << endl;
+    }
+  }
+}
+
 font &get_default_font() {
   static font* p = new font("FONT");
   return *p;
@@ -533,16 +764,19 @@ typedef dip<20, 6, false> DIP40;
 typedef dip<20, 6, true> DIP40V;
 
 int main() {
+  net &vdd(*(new net("VDD"))), &gnd(*(new net("GND"))), &x(*(new net("X")));
+
+  #if 1
   for (unsigned i = 0; i < 8; ++i) {
     track *t = new track(0, 1.0/20.0);
-    t->add_point(0.1*i, 0.15).add_point(0.1*i, 1.0);
+    t->add_point(0.1*i, 0.15).add_point(0.1*i,0.5).add_point(0.1*i, 1.0);
 
     via *v = new via(point(0.1*i, 1.0), 0.08, 0.035);
   }
 
   for (unsigned i = 0; i < 8; ++i) {
     track *t = new track(0, 1.0/20.0);
-    t->add_point(0.1*i, -0.15).add_point(0.1*i, -1.0);
+    t->add_point(0.1*i, -0.15).add_point(0.1*i,-0.5).add_point(0.1*i, -1.0);
 
     via *v = new via(point(0.1*i, -1.0), 0.08, 0.035);
   }
@@ -552,7 +786,13 @@ int main() {
   
   new text(get_default_font(), LAYER_CU0, point(0, -1.5), "Aa0", 1.0/16);
 
-  new DIP16("U0", point(0, -0.15));
+  DIP16 &u0(*(new DIP16("U0", point(0, -0.15))));
+
+  vdd.add_wire(u0.get_pin("8"));
+  gnd.add_wire(u0.get_pin("16"));
+  gnd.add_wire(u0.get_pin("1"));
+  gnd.add_wire(u0.get_pin("2"));
+  gnd.add_wire(u0.get_pin("3"));
 
   new R3("A0", point(0.7, 0.5));
   new R6("B1", point(0.7, -0.5));
@@ -562,6 +802,32 @@ int main() {
   (new track(1, 1/20.0))->add_point(1.7, -0.7).add_point(1.7, -0.6);
 
   new DIP40("U1", point(0, 1.5));
+
+  (new track(0, 1/20.0))->add_point(0,-1).add_point(0.1,-1).add_point(0.2,-1);
+  (new track(0, 1/20.0))->add_point(0, 0.15).add_point(0,-0.15);
+  
+  #else
+
+  R3 *r0 = new R3("A0", point(0, 0));
+  Dl<1, false> *d0 = new Dl<1, false>("D0", point(0.4, 0));
+  (new track(1, 1/20.0))->add_point(0.3,0).add_point(0.4,0);
+  (new track(0, 1/20.0))->add_point(0.3,0).add_point(0.3,-1);
+  (new via(point(0.3, -1), 0.08, 0.035));
+  (new track(1, 1/20.0))->add_point(-1,-1).add_point(0.3,-1).add_point(1,-1);
+
+  vdd.add_wire(r0->get_pin("0"));
+  x.add_wire(d0->get_pin("0"));
+  gnd.add_wire(d0->get_pin("1"));
+
+  #endif
+
+  wire::expand_nets();  
+  wire::check_nets();
+  // net::print_nets();
+
+  //gnd.mark();
+  //vdd.mark();
+  x.mark();
   
   {
     ofstream gfile("dump.cu0.grb");

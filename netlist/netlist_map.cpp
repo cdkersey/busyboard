79bb7d847bc50cd9a98f46e4369d8c962716fa99
chad <chad@816618ff-971e-4f3c-9917-b646dc1eeecf>
2015-08-30 10:59:18 +0000

    Updated netlist gen and docs.
    git-svn-id: http://www.cdkersey.com/log/2015.08.21/eeprom_prog@1707 816618ff-971e-4f3c-9917-b646dc1eeecf

--- netlist/netlist_map.cpp
+++ netlist/netlist_map.cpp
@@ -10,6 +10,11 @@ using namespace std;
 
 map<string, set<int> > vdd, gnd; // Pins designated as rails
 map<string, vector<int> > pinmap; // device -> pin numbers in CHDL order
+map<string, string> dmap; // Designator codes for various devices
+map<string, int> bom_count; // designator class to count
+map<string, string> bom; // designator to dev type
+map<string, set<string> > rbom; // dev type to designator set
+map<string, vector<pair<string, int> > > netlist; // net to designator and pin
 
 string read_chdl_module(istream &in) {
   string r; char c;
@@ -51,9 +56,93 @@ void read_pmap() {
   }
 }
 
+void read_tmap() {
+  ifstream in("TMAP");
 
+  while (!!in) {
+    string dev = read_chdl_module(in), des;
+    in >> des;
+    if (!in) break;
+    dmap[dev] = des;
+    cout << "dmap[\"" << dev << "\"] = " << des << ';' << endl;
+  }
+}
+
+string bom_new(string dev) {
+  bom_count[dmap[dev]]++;
+
+  int idx = bom_count[dmap[dev]];
+  ostringstream des;
+  des << dmap[dev] << idx;
+
+  bom[des.str()] = dev;
+  rbom[dev].insert(des.str());
+
+  return des.str();
+}
+
+void add_global_net(map<string, set<int> > &g, string net) {
+  for (auto &x : g) {
+    for (auto &des : rbom[x.first]) {
+      for (auto pin : x.second) {
+	netlist[net].push_back(make_pair(des, pin));
+        cout << net << ", add " << des << " pin " << pin << endl;
+      }
+    }
+  }
+}
+
+void read_netlist(string filename) {
+  ifstream in(filename);
+
+  // Read in the netlist
+  while (!!in) {
+    string dev = read_chdl_module(in);
+    if (!in) break;
+    string des = bom_new(dev);
+    cout << "Got " << dev << ", " << des << endl;
+    int idx = 0;
+    while (!!in && in.peek() != '\n') {
+      string net;
+      in >> net;
+      netlist[net].push_back(make_pair(des, pinmap[dev][idx]));
+      cout << net << ", add " << des << " pin " << pinmap[dev][idx] << endl;
+      idx++;
+    }
+  }
+
+  // Add the vdd and gnd pins to the netlist
+  add_global_net(vdd, "vdd");
+  add_global_net(gnd, "gnd");
+}
+
+void dump_bom() {
+  ostream &out(cout);
+  for (auto &x : rbom) {
+    cout << x.first << ':';
+    for (auto &y : x.second)
+      cout << ' ' << y;
+    cout << endl;
+  }
+}
+
+void dump_netlist() {
+  ostream &out(cout);
+
+  for (auto &x : netlist) {
+    out << x.first;
+    for (auto &y : x.second)
+      out << ' ' << y.first << ' ' << y.second;
+    out << endl;
+  }
+}
 
 int main(int argc, char **argv) {
   read_pmap();
+  read_tmap();
   read_netlist("final.nand");
+  cout << "=== BOM ===" << endl;
+  dump_bom();
+  cout << "=== NETS ===" << endl;
+  dump_netlist();
 }

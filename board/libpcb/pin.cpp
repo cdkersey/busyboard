01d696e2e19dedc2e8bcf1a94486216266169819
cdkersey <chad@cdkersey.com>
2015-10-20 23:12:59 -0600

    libpcb lives in another repo now.

--- board/libpcb/pin.cpp
+++
@@ -1,9 +0,0 @@
-#include "pin.h"
-
-using namespace libpcb;
-using namespace std;
-
-void libpcb::pin::get_points(set<pair<point, int> > &s) {
-  for (auto x : l)
-    s.insert(make_pair(p, (int)x - LAYER_CU0));
-}
